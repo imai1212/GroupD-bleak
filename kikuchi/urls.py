01b365e5e525b72100a9ab5a343079d5c3a6e2c1
Darzi-Rin <ksw2070039@stu.o-hara.ac.jp>
2021-09-21 14:43:31 +0900

    ブランチ公開

--- kikuchi/urls.py
+++ kikuchi/urls.py
@@ -1,7 +1,14 @@
 from django.urls import path
 from . import views
 
+
 app_name = 'kikuchi'
-urlpattern = [
-    
+urlpatterns = [
+    path('', views.IndexView.as_view(), name="index"),
+    path('inquiry/', views.InquiryView.as_view(), name="inquiry"),
+    path('blog_list/', views.BlogListView.as_view(), name="blog_list"),
+    path('blog_detail/<int:pk>/', views.BlogDetailView.as_view(), name="blog_detail"),
+    path('blog_create/', views.BlogCreateView.as_view(), name="blog_create"),
+    path('blog_update/<int:pk>/', views.BlogUpdateView.as_view(), name="blog_update"),
+    path('blog_delete/<int:pk>/', views.BlogDeleteView.as_view(), name="blog_delete"),
 ]
--- kikuchi/views.py
+++ kikuchi/views.py
@@ -1,3 +1,83 @@
 from django.shortcuts import render
+import logging
+from django.urls import reverse_lazy
+from django.views import generic
+from django.contrib import messages
+from .forms import InquiryForm
+from django.contrib.auth.mixins import LoginRequiredMixin
+from .models import Kikuchi
+from .forms import InquiryForm, BlogCreateForm
 
-# Create your views here.
+logger = logging.getLogger(__name__)
+
+
+class IndexView(generic.TemplateView):
+    template_name = "index.html"
+
+
+class InquiryView(generic.FormView):
+    template_name = "inquiry.html"
+    form_class = InquiryForm
+    success_url = reverse_lazy('kikuchi:inquiry')
+
+    def form_valid(self, form):
+        form.send_email()
+        messages.success(self.request, 'メッセージを送信しました。')
+        logger.info('Inquiry sent by {}'.format(form.cleaned_data['name']))
+        return super().form_valid(form)
+
+
+class BlogListView(LoginRequiredMixin, generic.ListView):
+    model = Kikuchi
+    template_name = 'blog_list.html'
+    paginate_by = 2
+
+    def get_queryset(self):
+        blogs = Blog.objects.filter(user=self.request.user).order_by('-created_at')
+        return blogs
+
+class BlogDetailView(LoginRequiredMixin, generic.DetailView):
+    model = Blog
+    template_name = 'blog_detail.html'
+
+class BlogCreateView(LoginRequiredMixin, generic.CreateView):
+    model = Kikuchi
+    template_name = 'blog_create.html'
+    form_class = BlogCreateForm
+    success_url = reverse_lazy('kikuchi:blog_list')
+
+    def form_valid(self, form):
+        diary = form.save(commit=False)
+        diary.user = self.request.user
+        diary.save()
+        messages.success(self.request, '日記を作成しました。')
+        return super().form_valid(form)
+
+    def form_invalid(self, form):
+        messages.error(self.request, "日記の作成に失敗しました。")
+        return super().form_invalid(form)
+
+class BlogUpdateView(LoginRequiredMixin, generic.UpdateView):
+    model = Kikuchi
+    template_name = 'blog_update.html'
+    form_class = BlogCreateForm
+
+    def get_success_url(self):
+        return reverse_lazy('kikuchi:blog_detail', kwargs={'pk': self.kwargs['pk']})
+
+    def form_valid(self, form):
+        messages.success(self.request, '日記を更新しました。')
+        return super().form_valid(form)
+
+    def form_invalid(self, form):
+        messages.error(self.request, "日記の更新に失敗しました。")
+        return super().form_invalid(form)
+
+class BlogDeleteView(LoginRequiredMixin, generic.DeleteView):
+    model = Kikuchi
+    template_name = 'blog_delete.html'
+    success_url = reverse_lazy('kikuchi:blog_list')
+
+    def delete(self, request, *args, **kwargs):
+        messages.success(self.request, "日記を削除しました。")
+        return super().delete(request, *args, **kwargs)
