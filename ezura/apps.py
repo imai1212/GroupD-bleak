0707bedeaf3c8a3bb2ea1abcf643c1b94d6704d3
imai1212 <ksw2070113@stu.o-hara.ac.jp>
2021-09-21 12:14:09 +0900

    mainブランチの基盤[完成] 代表は菊池

---
+++ ezura/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class EzuraConfig(AppConfig):
+    name = 'ezura'
---
+++ hinata/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class HinataConfig(AppConfig):
+    name = 'hinata'
